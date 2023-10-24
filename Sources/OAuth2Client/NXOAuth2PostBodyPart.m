d3f5c1cf82be0a75b06f0db7a1d5978c4402a4eb
Alex Shepard <alex@meatfreezer.com>
2023-10-24 11:37:19 -0700

    avoid xcode build complaint
    Multiple methods named 'contentType' found with mismatched result, parameter type or attributes

--- Sources/OAuth2Client/NXOAuth2PostBodyPart.m
+++ Sources/OAuth2Client/NXOAuth2PostBodyPart.m
@@ -43,7 +43,8 @@
     } else if ([content isKindOfClass:[NSData class]]) {
         return [self initWithName:name dataContent:content];
     } else if ([content isKindOfClass:[NXOAuth2FileStreamWrapper class]]) {
-        return [self initWithName:name streamContent:[content stream] streamLength:[content contentLength] fileName:[content fileName] contentType:[content contentType]];
+        NXOAuth2FileStreamWrapper *fsContent = (NXOAuth2FileStreamWrapper *)content;
+        return [self initWithName:name streamContent:[fsContent stream] streamLength:[fsContent contentLength] fileName:[fsContent fileName] contentType:[fsContent contentType]];
     } else {
         NSAssert1(NO, @"NXOAuth2PostBodyPart with illegal type:\n%@", [content class]);
         return nil;
